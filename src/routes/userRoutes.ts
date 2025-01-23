b73678d7882bb0c7114c25a64e3ff5c49d9f5b84
Itachi-20 <neel.udvadia@merillife.com>
2025-01-23 17:56:24 +0530

    created new categories controller

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import UserController from "../controllers/userControllers";
 import ProductController from "../controllers/productController";
+import CategoriesController from '../controllers/categoriesController'
 
 const userRoutes = Router();
 const userController = new UserController();
 const productController = new ProductController();
+const categoriesController = new CategoriesController();
 
 userRoutes.post("/signup",userController.userRegister);
 userRoutes.post("/login",userController.userLogin);
 userRoutes.post("/products",productController.fetchProductsWithtypes);
+userRoutes.get("/categories",categoriesController.getCategories)
 
 export default userRoutes;
